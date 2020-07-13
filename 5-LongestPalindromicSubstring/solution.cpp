5142832c2b75b9ad4a0da3c7920f26803459cc58
duyanwei <duyanwei0702@gmail.com>
2020-07-13 23:35:05 +0800

    unit test of 5-LongestPalindromicSubstring

--- 5-LongestPalindromicSubstring/solution.cpp
+++ 5-LongestPalindromicSubstring/solution.cpp
@@ -1,9 +1,16 @@
 
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
+/**
+ * @brief brute force solution
+ * 
+ * @TIMECOMPLEXITY O(n^3)
+ * @SPACECOMPLEXITY O(1)
+ */
 class Solution {
 public:
     string longestPalindrome(string s) {
@@ -33,11 +40,49 @@ private:
     }
 };
 
+/**
+ * @brief dynamic programming
+ * 
+ */
+class Solution2 {
+public:
+    string longestPalindrome(string s) {
+        int N = s.size();
+        std::vector<std::vector<bool> > dp(N, std::vector<bool>(N, false));
+
+        int left = 0;
+        int right = 0;
+
+
+        for (int j = 0; j < N; j++)
+        {
+            for (int i = j; i >= 0; i--)
+            {
+                dp[i][j] = (s[i] == s[j]) && (j - i > 2 ? dp[i+1][j-1] : true);
+
+                if (dp[i][j] && (j - i) > (right - left))
+                {
+                    right = j;
+                    left = i;
+                }
+            }
+        }
+
+        return s.substr(left, right - left + 1u);
+    }
+};
+
 int main()
 {
     const std::string test = "flsuqzhtcahnyickkgtfnlyzwjuiwqiexthpzvcweqzeqpmqwkydhsfipcdrsjkefehhesubkirhalgnevjugfohwnlhbjfewiunlgmomxkafuuokesvfmcnvseixkkzekuinmcbmttzgsqeqbrtlwyqgiquyylaswlgfflrezaxtjobltcnpjsaslyviviosxorjsfncqirsjpkgajkfpoxxmvsyynbbovieoothpjgncfwcvpkvjcmrcuoronrfjcppbisqbzkgpnycqljpjlgeciaqrnqyxzedzkqpqsszovkgtcgxqgkflpmrikksaupukdvkzbltvefitdegnlmzeirotrfeaueqpzppnsjpspgomyezrlxsqlfcjrkglyvzvqakhtvfmeootbtbwfhqucbnuwznigoyatvkocqmbtqghybwrhmyvvuchjpvjckiryvjfxabezchynfxnpqaeampvaapgmvoylyutymdhvhqfmrlmzkhuhupizqiujpwzarnszrexpvgdmtoxvjygjpmiadzdcxtggwamkbwrkeplesupagievwsaaletcuxtpsxmbmeztcylsjxvhzrqizdmgjfyftpzpgxateopwvynljzffszkzzqgofdlwyknqfruhdkvmvrrjpijcjomnrjjubfccaypkpfokohvkqndptciqqiscvmpozlyyrwobeuazsawtimnawquogrohcrnmexiwvjxgwhmtpykqlcfacuadyhaotmmxevqwarppknoxthsmrrknu";
 
-    Solution ss;
-    const std::string result = ss.longestPalindrome(test);
-    std::cout << result << std::endl;
+    // brute force
+    Solution s1;
+    std::string result = s1.longestPalindrome(test);
+    std::cout << "s1 = " << result << std::endl;
+
+    // dynamic programming
+    Solution2 s2;
+    result = s2.longestPalindrome(test);
+    std::cout << "s2 = " << result << std::endl;
 }
